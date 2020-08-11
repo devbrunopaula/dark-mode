40cbef108bdb91696fe7b49f0cee91ce27b8c88c
Bruno Paula <brunopaula@dryserv.com>
2020-08-11 16:19:06 -0400

    MVP Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+const Navbar = ({ toggleMode, darkmode }) => {
   return (
-    <nav className="navbar">
+    <nav className='navbar'>
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
+      <div className='dark-mode__toggle'>
         <div
           onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          className={darkmode ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
-
-import "./styles.scss";
+import Charts from './components/Charts'
+import Navbar from './components/Navbar'
+import { useLocalStorage } from './hooks/useLocalStorage'
+import './styles.scss'
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
+  const [coinData, setCoinData] = useState([])
+  const [darkmode, setDarkMode] = useLocalStorage('dark-mode')
+
+  const toggleMode = (e) => {
+    e.preventDefault()
+    setDarkMode(!darkmode)
+  }
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
-    <div className={darkmode ? "dark-mode App" : "App"}>
-      <Navbar />
+    <div className={darkmode ? 'dark-mode App' : 'App'}>
+      <Navbar toggleMode={toggleMode} darkmode={darkmode} />
+
       <Charts coinData={coinData} />
     </div>
-  );
-};
+  )
+}
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
